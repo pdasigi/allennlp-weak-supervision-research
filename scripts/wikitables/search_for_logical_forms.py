d8fb17b61786c75ad145a3e4a2e38d6af16b0c91
pdasigi <pdasigi@cs.cmu.edu>
2018-11-27 16:45:03 -0800

    added multiprocessing

--- scripts/wikitables/search_for_logical_forms.py
+++ scripts/wikitables/search_for_logical_forms.py
@@ -5,9 +5,13 @@ import sys
 import os
 import argparse
 import gzip
+import logging
+import math
+from multiprocessing import Process
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir)))))
 
+from allennlp.common.util import JsonDict
 from allennlp.data.tokenizers import WordTokenizer
 from allennlp.data.dataset_readers.semantic_parsing.wikitables import util as wikitables_util
 
@@ -16,14 +20,15 @@ from weak_supervision.semparse.worlds import WikiTablesVariableFreeWorld
 from weak_supervision.semparse import ActionSpaceWalker
 
 def search(tables_directory: str,
-           input_examples_file: str,
+           data: JsonDict,
            output_path: str,
            max_path_length: int,
            max_num_logical_forms: int,
            use_agenda: bool,
            output_separate_files: bool) -> None:
-    data = [wikitables_util.parse_example_line(example_line) for example_line in
-            open(input_examples_file)]
+    print(f"Starting search with {len(data)} instances", file=sys.stderr)
+    executor_logger = logging.getLogger('weak_supervision.semparse.executors.wikitables_variable_free_executor')
+    executor_logger.setLevel(logging.ERROR)
     tokenizer = WordTokenizer()
     if output_separate_files and not os.path.exists(output_path):
         os.makedirs(output_path)
@@ -88,6 +93,26 @@ if __name__ == "__main__":
                         action="store_true", help="""If set, the script will output gzipped
                         files, one per example. You may want to do this if you;re making data to
                         train a parser.""")
+    parser.add_argument("--num-splits", dest="num_splits", type=int, default=0,
+                        help="Number of splits to make of the data, to run as many processes (default 0)")
     args = parser.parse_args()
-    search(args.table_directory, args.data_file, args.output_path, args.max_path_length,
-           args.max_num_logical_forms, args.use_agenda, args.output_separate_files)
+    input_data = [wikitables_util.parse_example_line(example_line) for example_line in
+                  open(args.data_file)]
+    if args.num_splits == 0 or len(input_data) <= args.num_splits or not args.output_separate_files:
+        search(args.table_directory, input_data, args.output_path, args.max_path_length,
+               args.max_num_logical_forms, args.use_agenda, args.output_separate_files)
+    else:
+        chunk_size = math.ceil(len(input_data)/args.num_splits)
+        start_index = 0
+        for i in range(args.num_splits):
+            if i == args.num_splits - 1:
+                data_split = input_data[start_index:]
+            else:
+                data_split = input_data[start_index:start_index + chunk_size]
+            start_index += chunk_size
+            process = Process(target=search, args=(args.table_directory, data_split,
+                                                   args.output_path, args.max_path_length,
+                                                   args.max_num_logical_forms, args.use_agenda,
+                                                   args.output_separate_files))
+            print(f"Starting process {i}", file=sys.stderr)
+            process.start()
